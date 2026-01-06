7b23c88b88719d95b1c1da9ce5b742dc23315a7b
SAKSHI KESARWANI <sakshikesarwani2028@gmail.com>
2026-01-07 05:08:37 +0530

    Update Login.js

--- frontend/src/landing_page/Signup/Login.js
+++ frontend/src/landing_page/Signup/Login.js
@@ -32,7 +32,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/auth/login",
+        "https://zerodhaclone-1-y2ot.onrender.com/auth/login",
         {
           ...inputValue,
         },
