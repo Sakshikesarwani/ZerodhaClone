bd177b361d0a6f422886c9437fa6495784a94de9
SAKSHI KESARWANI <sakshikesarwani2028@gmail.com>
2026-01-07 06:27:22 +0530

    Update Menu.js

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -29,7 +29,7 @@ const Menu = () => {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3002/auth", {}, { withCredentials: true })
+      .post("https://zerodhaclone-1-y2ot.onrender.com/auth", {}, { withCredentials: true })
       .then((res) => setUsername(res.data.user || "User"))
       .catch(() => setUsername("User"));
   }, []);
@@ -46,7 +46,7 @@ const Menu = () => {
 
   const handleLogout = () => {
     removeCookie("token", { path: "/" });
-    window.location.href = "http://localhost:3000";
+    window.location.href = "https://zerodhaclone-2-szyv.onrender.com";
   };
 
   return (
